1aa60bf2ecabc27aa52670a22d523b20b75fd25b
Vilsol <vilsol2000@gmail.com>
2014-04-09 00:48:11 +0300

    Make a centralised config file

---
+++ src/main/java/me/vaqxine/Config.java
@@ -0,0 +1,27 @@
+package me.vaqxine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Config {
+
+    public static List<String> us_public_servers = new ArrayList<String>(Arrays.asList("US-1", "US-2", "US-3", "US-4", "US-11"));
+    public static List<String> us_private_servers = new ArrayList<String>(Arrays.asList("US-9", "US-10"));
+    public static List<String> br_servers = new ArrayList<String>(Arrays.asList("BR-1"));
+    
+    public static final int transfer_port = 6427;
+    public static final String Hive_IP = "72.20.40.38";
+
+    public static final int SQL_port = 7447;
+    public static final String sql_user = "slave_3XNZvi";
+    public static final String sql_password = "SgUmxYSJSFmOdro3";
+    public static final String sql_url = "jdbc:mysql://" + Hive_IP + ":" + SQL_port + "/dungeonrealms";
+
+    public static final int FTP_port = 21;
+    public static final String ftp_user = "agent";
+    public static final String ftp_pass = "9bgsMKsknkJ6OY"; 
+    
+    public static final String version = "1.6";
+    
+}
