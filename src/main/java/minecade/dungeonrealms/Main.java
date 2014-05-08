b93794fac1db7c22e2d9bb09c1e8d829fafa56ab
Vilsol <vilsol2000@gmail.com>
2014-05-08 18:26:14 +0100

    Adding Uno to masters list

--- src/main/java/minecade/dungeonrealms/Main.java
+++ src/main/java/minecade/dungeonrealms/Main.java
@@ -107,7 +107,7 @@ public class Main extends JavaPlugin implements Listener {
     public static Logger log;
 
     private static List<String> devs = Arrays.asList("Vilsol", "iFamasssxD", "Vaquxine");
-    private static List<String> masters = Arrays.asList("Bradez1571", "felipepcjr", "mayley1996");
+    private static List<String> masters = Arrays.asList("Bradez1571", "felipepcjr", "mayley1996", "UnoWild", "Vapeorz");
 
     public void onEnable() {
         plugin = this;
