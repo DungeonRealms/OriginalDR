9af389c09abf725a236107490cbebebc5d222380
Azubuso <azubuso@globaldevteam.net>
2014-06-18 23:34:38 +0200

    Added 'option' to save to db as well for clearshops command.

--- src/main/java/minecade/dungeonrealms/ShopMechanics/commands/CommandClearsShops.java
+++ src/main/java/minecade/dungeonrealms/ShopMechanics/commands/CommandClearsShops.java
@@ -12,24 +12,29 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class CommandClearsShops implements CommandExecutor {
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		Player pl = (Player)sender;
 		if (pl == null) return true;
-		
+
 		if (!pl.isOp() || !PermissionMechanics.isGM(pl.getName())) {
 			pl.sendMessage(ChatColor.RED + "You don't have the required permission for this command.");
 			return true;
 		}
 		
+		if (args.length > 1) return true;
+
 		if (!ShopMechanics.inverse_shop_owners.isEmpty()) {
 			ShopMechanics.saveOpenShopsToCollBin();
-			new BukkitRunnable() {
-				public void run() {
-					ShopMechanics.uploadAllCollectionBinData(); // Lets save it to database as well.
-				}
-			}.runTaskLater(Main.plugin, 20L * 7);
+			if (args[0].equalsIgnoreCase("stdb")) { // SaveToDatabase
+				new BukkitRunnable() {
+					public void run() {
+						ShopMechanics.uploadAllCollectionBinData(); // Lets save it to database as well. 
+						// (Note you wont be able to actually grab it from the bin directly after without downloading data from db.)
+					}
+				}.runTaskLater(Main.plugin, 20L * 2);
+			}
 			pl.sendMessage(ChatColor.GREEN + "Check console for more information.");
 		}
 		return true;
